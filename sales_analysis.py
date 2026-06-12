39d015cabbbde74a74f3e5363d106a6f8d8fb9e9
Andre Virgilli <andrevirgilio@gmail.com>
2026-06-12 14:40:21 -0300

    Add advanced aggregations with pandas

--- sales_analysis.py
+++ sales_analysis.py
@@ -13,3 +13,26 @@ print(df[df["valor"] > 150])
 
 print("\n=== TOTAL SALES BY CITY ===")
 print(df.groupby("cidade")["valor"].sum())
+
+print("\n=== SORT BY SALES DESC ===")
+print(df.sort_values("valor", ascending=False))
+
+print("\n=== SALES WITH COMMISSION ===")
+df["commission"] = df["valor"] * 0.10
+print(df)
+
+print("\n=== NULL VALUES ===")
+print(df.isnull().sum())
+
+print("\n=== CLEAN DATA ===")
+clean_df = df.dropna()
+print(clean_df)
+
+result = (df.groupby("cidade")
+            .agg(total_sales  = ("valor", "sum"),
+                 max_sale     = ("valor", "max"),
+                 min_sale     = ("valor", "min")
+      )
+);
+
+print(result)
